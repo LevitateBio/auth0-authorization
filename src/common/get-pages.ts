97498029a6124eecdd91dcd162b54417cb4f9d1d
hmc-cs-bbaker <bbaker@cs.hmc.edu>
2021-06-11 15:13:35 -0700

    fix another off by one

--- src/common/get-pages.ts
+++ src/common/get-pages.ts
@@ -16,7 +16,7 @@ export function getPages(url: string, accessToken: string, options: Options) {
       }
     }).then((result: any) => {
       const total_pages = Math.ceil(result.total / options.perPage);
-      if (total_pages > page + 1) {
+      if (total_pages > page) {
         return getPaged(page + 1).then((p: any) => {
           result.users = result.users.concat(p.users);
           return result;
